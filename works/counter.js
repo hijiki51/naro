9c0fe083e934cf8dc938ad5e993746ebb294c91a
hijiki51 <hibiki0719euph@gmail.com>
2021-05-23 16:04:26 +0900

    add reset

--- works/counter.js
+++ works/counter.js
@@ -1,6 +1,14 @@
 let count = 0;
 const countUp = () => {
   count++;
+  write();
+};
+
+const reset = () => {
+  count = 0;
+  werite();
+};
+const write = () => {
   const countElement = document.querySelector("#count");
   countElement.innerText = "回数: " + count;
 };
